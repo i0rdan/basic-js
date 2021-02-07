bd15981e94a5e576decd620422bbd1d21d706a1f
Evgeny Kazusev <55188582+i0rdan@users.noreply.github.com>
2021-02-07 13:48:55 +0300

    feat: add code

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,7 +1,35 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function repeater(/* str, options */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
-};
+module.exports = function repeater(str, options) {
+  let result = '';
+
+  if (options.separator == undefined) {
+    options.separator = '+';
+  }
+  if (options.additionSeparator == undefined) {
+    options.additionSeparator = '|';
+  } 
+  if (options.repeatTimes == undefined) {
+    return (result = str + options.addition);
+  } 
+  for (let i = 0; i < options.repeatTimes; i++) {
+    let additionSeparator = options.additionSeparator;
+    let additionString = '';
+
+    if (i == options.repeatTimes - 1) {
+      options.separator = '';
+    } 
+
+    for (let j = 0; j < options.additionRepeatTimes; j++) {
+      if (j == options.additionRepeatTimes - 1) {
+        additionSeparator = '';
+      }
+
+      additionString = additionString + options.addition + additionSeparator;
+    }
+
+    result = result + str + additionString + options.separator;
+  }
   
+  return result;
+};
